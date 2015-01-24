c9d5aa8bafa95e8d9ea89dbf5d7148f1d4375021
jonathanVR <jonathan.valdivia.r@gmail.com>
2015-01-24 17:19:57 -0400

    Branch: sp1_erp1.1 Feature: ERP1.1 - BugFixing Task: ERP-32 Autor: Jonathan Valdivia R. Descripcion: Agregando Par Entities

---
+++ src/main/java/com/bap/erp/modelo/par/ParEstado.java
@@ -0,0 +1,43 @@
+package com.bap.erp.modelo.par;
+
+import com.bap.erp.modelo.ERP;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "par_estado")
+public class ParEstado implements Serializable {
+
+    private static final long serialVersionUID = ERP.serialVersionIdErp;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo", nullable = false)
+    private String codigo;
+    @Column(name = "descripcion", nullable = false)
+    private String descripcion;
+
+    public ParEstado() {
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+}
